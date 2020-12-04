3051906b5e78cad46ca824c81f80ff8c53ae167b
vishalbalakrishnan <vishal.bala07@gmail.com>
2020-12-04 22:31:57 +0530

    [6kyu] Sum of parts

--- sum_of_parts.js
+++ sum_of_parts.js
@@ -1,12 +1,5 @@
 function partsSums(ls) {
-    let length = ls.length
-    let array = ls
-    let last = []
-    for (let i = 0; i < length; i++) {
-        last.push(array.reduce((acc, val) => acc + val))
-        array.splice(0, 1)
-    }
-    last.push(0)
-    return last
+    let x = ls.reduce((acc, val) => acc + val, 0)
+    return [x].concat(ls.map(e => x -= e ))
 }
-console.log(partsSums([1, 2, 3, 4, 5, 6]));
+

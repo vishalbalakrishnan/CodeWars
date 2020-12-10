cda2526568fb9216a7a22f41eed4f10b62ff2f0e
vishalbalakrishnan <vishal.bala07@gmail.com>
2020-12-10 17:36:06 +0530

    8[kyu] Check the exam

--- check_the_exam.js
+++ check_the_exam.js
@@ -1,1 +1,9 @@
-//code it
+function checkExam(array1, array2) {
+    let x = 0;
+    for (let i = 0; i < array1.length; i++) {
+        array1[i] == array2[i] ? x += 4 : !array2[i] ? x = x : x -= 1;
+    }
+    return x < 0 ? 0 : x
+}
+
+// checkExam = (x, y) => (x = y.reduce((s, e, i) => s + (e === x[i] ? 4 : e === '' ? 0 : -1), 0)) > 0 ? x : 0;
